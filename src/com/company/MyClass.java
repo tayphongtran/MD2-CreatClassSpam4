4eedfd0e94b6c83332a3629639c6418695e27065
anhtay <tayphongtran@gmail.com>
2022-03-26 22:55:40 +0700

    dusty 4

---
+++ src/com/company/MyClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        Summon summon = new Summon();
+        summon.Brack();
+        System.out.println(summon + " sao");
+    }
+}
---
+++ src/com/company/Summon.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Summon {
+    public void Brack(){
+        System.out.println("demasia");
+    }
+    void String(){
+        System.out.println("help");
+    }
+}
